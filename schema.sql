1156764b6a711af0c044e2c19e7ea09f0358f619
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-04-24 17:10:42 -0500

    CREATE TABLE employee

--- schema.sql
+++ schema.sql
@@ -17,3 +17,19 @@ CREATE TABLE role (
    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
+CREATE TABLE employee (
+   id INT NOT NULL AUTO_INCREMENT,
+   first_name VARCHAR(30) NOT NULL,
+   last_name VARCHAR(30) NOT NULL,
+   role_id INT NOT NULL,
+   manager_id INT NULL,
+   PRIMARY KEY (id),
+   FOREIGN KEY (role_id) REFERENCES role(id),
+   FOREIGN KEY (manager_id) REFERENCES employee(id)
+
+);
+
+SELECT * FROM employees;
+SELECT * FROM roles;
+SELECT * FROM departments;
+© 2021 GitHub, Inc.
