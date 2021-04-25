d7abbab04a8a6d1379c623c6cfec4b9367c7ecf5
fadhaa89 <fadhaaabdullah8991@gmail.com>
2021-04-25 14:07:49 -0500

    connection.query is added to index.js along with questions for eachone

--- index.js
+++ index.js
@@ -100,3 +100,204 @@ function addDepartment() {
             viewDepartments();
         })
 }
+
+function addRole() {
+    inquirer.prompt([{
+                name: 'name',
+                type: 'input',
+                message: 'Enter the role name: '
+            },
+            {
+                name: 'salary',
+                type: 'number',
+                message: 'Enter the salary: ',
+                validate: salary => {
+                    if (salary) {
+                        return true;
+                    } else {
+                        console.log('Please enter a number!');
+                        return false;
+                    }
+                }
+            },
+            {
+                name: 'department',
+                type: 'list',
+                message: 'Select the department:',
+                choices: getDepartments()
+            }
+        ])
+        .then(response => {
+            var responseID = 0;
+
+            connection.query('SELECT id FROM department WHERE name = ?', [response.department], (error, result) => {
+
+                if (error) throw error;
+                result.forEach(id => {
+                    responseID = id.id;
+                })
+
+                connection.query('INSERT INTO role SET ?', {
+                    title: response.name,
+                    salary: response.salary,
+                    department_id: responseID
+                }, (error, result) => {
+                    if (error) throw error;
+                })
+
+                viewRoles();
+            })
+        })
+}
+
+function addEmployee() {
+    inquirer.prompt([{
+                name: 'firstName',
+                type: 'input',
+                message: 'Enter the employee first name: '
+            },
+            {
+                name: 'lastName',
+                type: 'input',
+                message: 'Enter the employee last name: '
+            },
+            {
+                name: 'role',
+                type: 'list',
+                message: 'Select the role:',
+                choices: getRoles()
+            },
+            {
+                name: 'manager',
+                type: 'list',
+                message: 'Select the manager:',
+                choices: getEmployees()
+            }
+        ])
+        .then(response => {
+            var roleID = 0;
+            var managerID = 0;
+
+            connection.query('SELECT id FROM role WHERE title = ?', [response.role], (error, result) => {
+                if (error) throw error;
+
+                result.forEach(id => {
+                    roleID = id.id;
+                })
+
+                var managerFirstName = "";
+
+                for (var i = 0; i < response.manager.length; i++) {
+                    if (response.manager.charAt(i) === " ") {
+                        break;
+                    } else {
+                        managerFirstName += response.manager.charAt(i);
+                    }
+                }
+
+                connection.query('SELECT id FROM employee WHERE first_name = ?', [managerFirstName], (error, nextResult) => {
+                    if (error) throw error;
+
+                    nextResult.forEach(id => {
+                        managerID = id.id;
+                    })
+
+                    connection.query('INSERT INTO employee SET ?', {
+                        first_name: response.firstName,
+                        last_name: response.lastName,
+                        role_id: roleID,
+                        manager_id: managerID
+                    }, (error, result) => {
+                        if (error) throw error;
+                    })
+
+                    viewEmployees();
+                })
+            })
+        })
+}
+
+function updateEmployee() {
+    inquirer.prompt([{
+                name: 'employee',
+                type: 'number',
+                message: 'Enter the employee ID of the employee you wish to update:'
+            },
+            {
+                name: 'role',
+                type: 'number',
+                message: 'Enter the role ID you wish to update the employee to:'
+            }
+        ])
+        .then(response => {
+            connection.query('UPDATE employee SET role_id = ? WHERE id = ? ', [response.role, response.employee], (error, result) => {
+                if (error) throw error;
+
+                viewEmployees();
+            })
+        })
+}
+
+function getDepartments() {
+    let departments = [];
+    connection.query('SELECT name FROM department', (error, response) => {
+        if (error) throw error;
+
+        response.forEach(department => {
+            departments.push(department.name);
+        })
+    })
+
+    return departments;
+}
+
+function getRoles() {
+    let roles = [];
+    connection.query('SELECT title FROM role', (error, response) => {
+        if (error) throw error;
+
+        response.forEach(role => {
+            roles.push(role.title);
+        })
+    })
+
+    return roles;
+}
+
+function getEmployees() {
+    let firstNames = [];
+    let lastNames = [];
+    let employees = [];
+
+    connection.query('SELECT first_name FROM employee', (error, response) => {
+        if (error) throw error;
+
+        response.forEach(first_name => {
+            firstNames.push(first_name.first_name);
+        })
+
+        connection.query('SELECT last_name FROM employee', (error, response) => {
+            if (error) throw error;
+
+            response.forEach(last_name => {
+                lastNames.push(last_name.last_name);
+            })
+
+            for (var i = 0; i < firstNames.length; i++) {
+                employees[i] = firstNames[i] + " " + lastNames[i];
+            }
+        })
+    })
+
+    return employees;
+}
+
+
+connection.connect(err => {
+    if (err) throw err;
+
+    console.log('connected as id ' + connection.threadId + '\n');
+    console.log('WELCOME TO EMPLOYEE TRACKER!' + '\n');
+
+    mainMenu();
+})
